94a38ac559e843b23eae5750570c7bcdcb902aea
EthanDBer <crimezjp@163.com>
2021-08-07 22:17:26 +0800

    add

--- Traditional-Algorithms/LeetCode103.cpp
+++ Traditional-Algorithms/LeetCode103.cpp
@@ -1,3 +1,4 @@
+//层次遍历
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -35,6 +36,49 @@ public:
                 }
             }
             ans.push_back(tmp);
+            level++;
+        }
+        return ans;
+    }
+};
+
+
+//层次遍历
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(root == nullptr) return {};
+        
+        queue<TreeNode*> q;
+        vector<vector<int>>ans;
+        int level = 0;
+        q.push(root);
+        
+        while(!q.empty()){
+            vector<int>oneLevel;
+            for(int i = q.size(); i > 0; i--){
+                TreeNode* tmp = q.front();
+                q.pop();
+                oneLevel.push_back(tmp->val);
+                if(tmp->left != nullptr) q.push(tmp->left);
+                if(tmp->right != nullptr) q.push(tmp->right);
+            }
+            if(level % 2 == 0) ans.push_back(oneLevel);
+            else{
+                reverse(oneLevel.begin(), oneLevel.end());
+                ans.push_back(oneLevel);
+            }
             level++;
         }
         return ans;
