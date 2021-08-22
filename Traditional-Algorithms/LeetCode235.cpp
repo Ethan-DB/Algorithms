b2635bfca29c9c528ee14c0252ebea2d47f7d332
EthanDBer <crimezjp@163.com>
2021-08-22 23:43:15 +0800

    add

--- Traditional-Algorithms/LeetCode235.cpp
+++ Traditional-Algorithms/LeetCode235.cpp
@@ -22,3 +22,50 @@ public:
             return l ? l:r;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// DFS找出每个点的遍历结果，放入到vector中，然后将其中一个点的遍历结果放入到hash表中，
+// 从右向左搜索另外一个结果，比较在hash表中是否存在相同的值，返回即可
+// 时间复杂度O(h)，空间复杂度O(N)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> resP;
+        vector<TreeNode*> resQ;
+        dfs(root, p, resP);
+        dfs(root, q, resQ);
+        
+        unordered_map<int, TreeNode*>m;
+        for(auto u : resP){
+            m[u->val] = u;
+        }
+        
+        int len = resQ.size();
+        for(int i = len - 1; i >= 0; i--){
+            if(m.find(resQ[i]->val) != m.end()) return resQ[i];
+        }
+        return nullptr;
+    }
+    void dfs(TreeNode* root, TreeNode* p, vector<TreeNode*>& res){
+        if(root->val == p->val) {
+            res.push_back(root);
+            return;
+        }
+        res.push_back(root);
+        if(p->val < root->val){
+            dfs(root->left, p, res);
+        }else{
+            dfs(root->right, p, res);
+        }
+        return;
+    }
+};
