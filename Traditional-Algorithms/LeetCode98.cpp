08a5827357579e373faa9454ee33083fa5dc506e
EthanDBer <crimezjp@163.com>
2021-08-22 22:05:31 +0800

    add

--- Traditional-Algorithms/LeetCode98.cpp
+++ Traditional-Algorithms/LeetCode98.cpp
@@ -58,3 +58,36 @@ public:
         return true;
     }
 };
+
+// 当前思路最明确解法
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 中序遍历，但是不用保存所有的值
+class Solution {
+    TreeNode* prev;
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> s;
+        while(root != nullptr || !s.empty()){
+            while(root != nullptr){
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            if(prev && prev->val >= root->val) return false;
+            prev = root;
+            root = root->right;
+        }
+        return true;
+    }
+};
